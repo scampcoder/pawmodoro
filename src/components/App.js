9f6ef01097c5ee1328a721a4dae34d1a39b2d840
Victoria Kee <victoriakee95@gmail.com>
2021-12-02 14:28:10 -0600

    setup app as class comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../App.css';
 
-function App() {
-  return (
-    <main>
-      <h2>Pawmodoro Clock</h2>
-    </main>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <main>
+        <h2>Pawmodoro Clock</h2>
+      </main>
+    );
+  }
 }
 
 export default App;
