20d97858687f5e9544db2733e0985a6f6df534ad
Victoria Kee <victoriakee95@gmail.com>
2021-12-02 16:11:22 -0600

    add font awesome

--- src/components/MainTimerComponent.js
+++ src/components/MainTimerComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Timer extends React.Component {
     constructor(props) {
@@ -12,12 +13,19 @@ class Timer extends React.Component {
     
     render() {
         return (
-            <section className="main-timer">
-                {/*if we we are in a session display Work, if not display Chill*/}
-                <h4>{this.state.isSession === true ? "Work" : "Cat Nap"}</h4>
-                <span className="timer">{this.props.timerMinute}</span>
-                <span className="timer">:</span>
-                <span className="timer">{this.state.timerSecond < 10 ? '0' + this.state.timerSecond : this.state.timerSecond}</span>
+            <section>
+                <section className="main-timer">
+                    {/*if we we are in a session display Work, if not display Chill*/}
+                    <h4>{this.state.isSession === true ? "Work" : "Cat Nap"}</h4>
+                    <span className="timer">{this.props.timerMinute}</span>
+                    <span className="timer">:</span>
+                    <span className="timer">{this.state.timerSecond < 10 ? '0' + this.state.timerSecond : this.state.timerSecond}</span>
+                </section>
+                <section className="action-btns">
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </section>
             </section>
         )
     }
