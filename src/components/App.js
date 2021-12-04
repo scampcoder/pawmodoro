6d10db55c9ea5da36b143ec80f67a14eba0ae138
Victoria Kee <victoriakee95@gmail.com>
2021-12-03 18:53:25 -0600

    Create break controls

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import '../App.css';
-import Break from './BreakComponent';
-import Session from './SessionComponent';
-import Timer from './MainTimerComponent';
+import React from "react";
+import "../App.css";
+import Break from "./BreakComponent";
+import Session from "./SessionComponent";
+import Timer from "./MainTimerComponent";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,16 +10,39 @@ class App extends React.Component {
     this.state = {
       breakLength: 5,
       sessionLength: 25,
-      timerMinute: 25
-    }
+      timerMinute: 25,
+    };
+    this.increaseBreak = this.increaseBreak.bind(this);
+    this.decreaseBreak = this.decreaseBreak.bind(this);
   }
+
+  increaseBreak() {
+    this.setState((prevState) => {
+      return {
+        breakLength: prevState.breakLength + 1,
+      };
+    });
+  }
+
+  decreaseBreak() {
+    this.setState((prevState) => {
+      return {
+        breakLength: prevState.breakLength - 1,
+      };
+    });
+  }
+
   render() {
     return (
       <main>
         <h2>Pawmodoro</h2>
         <Timer timerMinute={this.state.timerMinute} />
         <section className="intervals-container">
-          <Break breakLength={this.state.breakLength} />
+          <Break
+            breakLength={this.state.breakLength}
+            increaseBreak={this.increaseBreak}
+            decreaseBreak={this.decreaseBreak}
+          />
           <Session sessionLength={this.state.sessionLength} />
         </section>
       </main>
--- src/components/BreakComponent.js
+++ src/components/BreakComponent.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 function Break(props) {
+    function decreaseCounter() {
+        if(props.breakLength === 1) {
+            return;
+        }
+        props.decreaseBreak();
+    }
+
+    function increaseCounter() {
+        if(props.breakLength === 30) {
+            return;
+        }
+        props.increaseBreak();
+    }
+
     return (
         <section>
             <h4>Cat Nap</h4>
             <section className="interval-container">
-                <button>-</button>
+                <button onClick={decreaseCounter}>-</button>
                 <p>{props.breakLength}</p>
-                <button>+</button>
+                <button onClick={increaseCounter}>+</button>
             </section>
         </section>
     );
